94d5bce5397013562ef1bd011b836fb994fef95e
marianna delrio <mariannadelrio@cis.vvc.edu>
2018-10-16 11:41:27 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,47 @@
+/*
+ File: Week8
+ Description: Functions Review
+ Author: Marianna Delrio
+ Email: Mariand3224@studnet.vvc.edu
+ Date:10/16/18
+*/
+
+
+#include<string>
 #include<iostream>
 
 
 using namespace std;
 
+ 
+int a(string m ) 
+ {  
+    int l =0;
+    for(int i =0; i < m.length(); i++)
+     { 
+    if(m.at(i)=='a' || m.at(i)=='A') 
+       
+     { 
+        l++;
+     }
+
+
+     }
+            return l; 
+ }
+
+
 
 int main()
 {
-    cout << "Hello World"<< endl;
+    string k = " johny apple seed";
+    cout <<a(k)  << endl;
 
     return 0;
 }
+
+
+
+
+
+
